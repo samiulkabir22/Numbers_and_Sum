32ba79d448c7f231ae9221591b1c6e0707fcf6b6
Mohammad Samiul Kabir <samiulkabir0522@gmail.com>
2025-10-19 21:58:04 +0600

    Implement function to calculate custom sum

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+int donno(int a,int b,int c){
+    
+    return a*100+(b*10)+c;
+     
+}
 
+int main() {
+  
+int a;
+int x,y,z;
+cin>>a;
+
+z=a%10;
+a=a/10;
+y=a%10;
+a=a/10;
+x=a%10;
+
+
+ int sum = donno(z,x,y) + donno(y,z,x)+ donno(x,z,y);
+ cout<<sum;
+
+}
